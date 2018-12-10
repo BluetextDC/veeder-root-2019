4d94f7f9fd7cd0d520ad956db31eaf1dc2a0d3a0
girishk <girishk@zealousweb.com>
2018-12-10 19:46:54 +0530

    folder all the templates and update 50-50 & 40-60 templates.

--- themes/custom/veeder/js/scripts.js
+++ themes/custom/veeder/js/scripts.js
@@ -169,15 +169,6 @@
         remove: false
     });
 
-    // Background Image to parent div start
-    jQuery(".bg-img img").each(function(i, elem) {
-        var img = jQuery(elem);
-        jQuery(this).hide();
-        jQuery(this).parent().css({
-            background: "url(" + img.attr("src") + ") no-repeat center center",
-        });
-    });
-
     /* parallax */
     $.fn.parallax = function () {
         var b = $(window).width();
