6bfb8b953fd1bb169a40043b95e455e533b4637c
girishk <girishk@zealousweb.com>
2018-12-19 19:47:21 +0530

    single autocomplete script.

--- modules/custom/viewfield_autocomplete/js/custom-script.js
+++ modules/custom/viewfield_autocomplete/js/custom-script.js
@@ -13,43 +13,20 @@
 
   var availableTags = productTerms.concat(solutionTerms);
   
-
-  function split( val ) {
-    return val.split( /,\s*/ );
-  }
-  function extractLast( term ) {
-    return split( term ).pop();
-  }
- 
-  $( "#edit-field-components-6-subform-field-3-column-taxonomy-view-0-arguments" )
-    // don't navigate away from the field on tab when selecting an item
-    .on( "keydown", function( event ) {
-      if ( event.keyCode === $.ui.keyCode.TAB &&
-          $( this ).autocomplete( "instance" ).menu.active ) {
-        event.preventDefault();
-      }
-    })
-    .autocomplete({
-      minLength: 0,
-      source: function( request, response ) {
-        // delegate back to autocomplete, but extract the last term
-        response( $.ui.autocomplete.filter(
-          availableTags, extractLast( request.term ) ) );
-      },
-      focus: function() {
-        // prevent value inserted on focus
-        return false;
-      },
-      select: function( event, ui ) {
-        var terms = split( this.value );
-        // remove the current input
-        terms.pop();
-        // add the selected item
-        terms.push( ui.item.value );
-        // add placeholder to get the comma-and-space at the end
-        terms.push( "" );
-        this.value = terms.join( ", " );
-        return false;
-      }
+  $( ".paragraph-type--3-column-taxonomy-listing .js-form-type-textfield input" ).autocomplete({
+    source: availableTags
+  });
+  $( ".paragraph-type--4-column-taxonomy-listing .js-form-type-textfield input" ).autocomplete({
+    source: availableTags
+  });
+  
+  
+  $(document).ajaxSuccess(function (event, xhr, settings) {
+    $( ".paragraph-type--3-column-taxonomy-listing .js-form-type-textfield input" ).autocomplete({
+      source: availableTags
+    });
+    $( ".paragraph-type--4-column-taxonomy-listing .js-form-type-textfield input" ).autocomplete({
+      source: availableTags
+    });
   });
 })(jQuery, window, Drupal);
