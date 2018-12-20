6505ab775e75cedde79bfc559f02f0844c8c73c4
girishk <girishk@zealousweb.com>
2018-12-20 17:21:19 +0530

    Updated custom module's JS

--- modules/custom/viewfield_autocomplete/js/custom-script.js
+++ modules/custom/viewfield_autocomplete/js/custom-script.js
@@ -1,31 +1,23 @@
 (function ($, window, Drupal) {
   'use strict';
 
-  if (drupalSettings.viewfield_autocomplete.product != null ) {
+  if (drupalSettings.viewfield_autocomplete.product != null && drupalSettings.viewfield_autocomplete.solution != null) {
     var productTerms = drupalSettings.viewfield_autocomplete.product;
-  }
-  if (drupalSettings.viewfield_autocomplete.solution != null ) {
     var solutionTerms = drupalSettings.viewfield_autocomplete.solution;
-  }
-  if (drupalSettings.viewfield_autocomplete.blog != null) {
-    var blogTerms = drupalSettings.viewfield_autocomplete.blog;
+    var availableTags = productTerms.concat(solutionTerms);
   }
 
-  var availableTags = productTerms.concat(solutionTerms);
-  
-  $( ".paragraph-type--3-column-taxonomy-listing .js-form-type-textfield input" ).autocomplete({
+  if (drupalSettings.viewfield_autocomplete.blog != null) {
+    var blogTerms = drupalSettings.viewfield_autocomplete.blog;
+    var availableTags = productTerms.concat(solutionTerms, blogTerms);
+  }
+
+  $( ".field--type-viewfield .js-form-type-textfield input" ).autocomplete({
     source: availableTags
   });
-  $( ".paragraph-type--4-column-taxonomy-listing .js-form-type-textfield input" ).autocomplete({
-    source: availableTags
-  });
-  
   
   $(document).ajaxSuccess(function (event, xhr, settings) {
-    $( ".paragraph-type--3-column-taxonomy-listing .js-form-type-textfield input" ).autocomplete({
-      source: availableTags
-    });
-    $( ".paragraph-type--4-column-taxonomy-listing .js-form-type-textfield input" ).autocomplete({
+    $( ".field--type-viewfield .js-form-type-textfield input" ).autocomplete({
       source: availableTags
     });
   });
